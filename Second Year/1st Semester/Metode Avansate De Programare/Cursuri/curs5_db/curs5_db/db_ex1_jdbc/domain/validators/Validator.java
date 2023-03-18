42da617b8686296be94c2b921aa1d6d16addb0d9
PaulaBeatrice <serbanpaula002@gmail.com>
2023-03-18 23:01:01 +0200

    .

--- Second Year/1st Semester/Metode Avansate De Programare/Cursuri/curs5_db/curs5_db/db_ex1_jdbc/domain/validators/Validator.java
+++
@@ -1,7 +0,0 @@
-package ro.ubbcluj.map.domain.validators;
-
-import  ro.ubbcluj.map.domain.validators.ValidationException;
-
-public interface Validator<T> {
-    void validate(T entity) throws ValidationException;
-}
